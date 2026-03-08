5de589b5b5d5dfd1c7c92613031da2c18522f504
anniekvangent <anniekvangent@gmail.com>
2026-03-08 18:23:30 +0100

    PCA analysis toegevoegd

--- worcliver/Feature selection.py
+++ worcliver/Feature selection.py
@@ -1,4 +1,5 @@
 #%%
+# import packages
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -12,6 +13,7 @@ from sklearn import feature_selection
 from sklearn import preprocessing
 from sklearn import neighbors
 from sklearn import svm
+from sklearn.ensemble import RandomForestClassifier
 
 from load_data import load_data
 
@@ -37,7 +39,7 @@ def scaling_data(data_train, data_test):
     return train_data_scaled, test_data_scaled
 
 #%%
-#feature selection
+#feature selection - amount of features
 train_data_scaled, test_data_scaled = scaling_data(data_train, data_test)
 # RFE object:
 svc = svm.SVC(kernel="linear")
@@ -57,5 +59,46 @@ plt.show()
 optimal = rfecv.n_features_
 print(optimal)
 
+#%%
+#principle component analysis
+from sklearn import decomposition
+
+pca = decomposition.PCA(n_components=optimal)
+pca.fit(train_data_scaled)
+X_train_pca = pca.transform(train_data_scaled)
+X_test_pca = pca.transform(test_data_scaled)
+
+print(X_test_pca.shape)
 
 # %%
+# feature selection - importance
+forest = RandomForestClassifier(n_estimators=100)
+
+forest.fit(train_data_scaled, classification_train)
+importances = forest.feature_importances_
+std = np.std([tree.feature_importances_ for tree in forest.estimators_],
+             axis=0)
+indices = np.argsort(importances)[::-1]
+
+# Print the feature ranking
+print("Feature ranking:")
+
+for f in range(train_data_scaled.shape[1]):
+    print("%d. feature %d (%f)" % (f + 1, indices[f], importances[indices[f]]))
+
+print(indices[0:optimal])
+
+# Plot the feature importances of the forest
+plt.figure()
+plt.title("Feature importances")
+plt.bar(range(train_data_scaled.shape[1]), importances[indices],
+       color="r", yerr=std[indices], align="center")
+plt.xticks(range(train_data_scaled.shape[1]), indices)
+plt.xlim([-1, train_data_scaled.shape[1]])
+plt.ylim([0, 0.09])
+plt.show()
+# %%
+
+# PCA doen voor feature extraction
+# relu voor neural network
+# laatste signoid of binary
