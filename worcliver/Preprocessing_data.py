68096620643e579809cf51bece7e4b0f8cd6f997
ZusGeijsen <z.geijsen@student.tudelft.nl>
2026-03-10 10:25:16 +0100

    splitsen in vaste groepen

--- worcliver/Preprocessing_data.py
+++ worcliver/Preprocessing_data.py
@@ -15,8 +15,10 @@ from sklearn.ensemble import RandomForestClassifier
 
 def preprocessing_data(data):
 #split data
-    data_train, data_test, classification_train, classification_test = model_selection.train_test_split(data.iloc[:,2:], data['label'])
-
+    #data_train, data_test, classification_train, classification_test = model_selection.train_test_split(data.iloc[:,2:], data['label'])
+data_train, data_test, classification_train, classification_test = train_test_split(
+    X, y, test_size=0.20, random_state=42, stratify=y #door 42 is een standaard random dan hou je de zelfde manier 
+)
 #%%
 # scaling data
     scaler = preprocessing.RobustScaler()
